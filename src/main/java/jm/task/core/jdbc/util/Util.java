9ea0e15e003e283c2fd1c71f97bdcc042d612bc1
Albertino274 <albertino274@mail.ru>
2025-01-23 08:39:31 +0300

    Настройка соединения

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,16 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    public static Connection getConnection() throws SQLException {
+        Connection conn = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/bd" ,
+                "root" ,
+                "root1"
+        );
+        return conn;
+    }
 }
